6e241088f5e3402aaed5f3324ee4590535aecb34
Chowdam Nanda kishore <nandalearner12@gmail.com>
2025-08-31 12:06:03 +0530

    Add files via upload

---
+++ Frontend/src/Components/Login.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import { login, register } from '../api';
+
+export default function Login({ setToken }) {
+    // Form logic here
+}
---
+++ Frontend/src/Components/TaskList.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function TaskList({ tasks, onToggle, onEdit, onDelete }) {
+    // Render tasks
+}
